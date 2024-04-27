c9ee14ca263b128883acd2fc1625f263b46c6d04
veronoicc <veronoicc@proton.me>
2024-04-27 17:20:28 +0000

    Make success better

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase", rename_all_fields = "lowercase")]
 #[serde(tag = "type")]
-pub enum Brest<D, C = u32> where D: Serialize, C: num_traits::PrimInt + Serialize {
-    Success {
-        data: D
-    },
+pub enum Brest<D, C = u32>
+where
+    D: Serialize,
+    C: num_traits::PrimInt + Serialize,
+{
+    Success(D),
     Error {
         message: String,
         #[serde(skip_serializing_if = "Option::is_none")]
@@ -18,134 +20,94 @@ pub enum Brest<D, C = u32> where D: Serialize, C: num_traits::PrimInt + Serializ
         message: String,
         #[serde(skip_serializing_if = "Option::is_none")]
         code: Option<C>,
-    }
+    },
 }
 
-impl<D, C> Brest<D, C> where D: Serialize, C: num_traits::PrimInt + Serialize {
+impl<D, C> Brest<D, C>
+where
+    D: Serialize,
+    C: num_traits::PrimInt + Serialize,
+{
     pub fn success(data: D) -> Self {
-        Self::Success {
-            data
-        }
+        Self::Success(data)
     }
 
     pub fn error<M: ToString>(message: M) -> Self {
         Self::Error {
             message: message.to_string(),
-            code: None
+            code: None,
         }
     }
-    
+
     pub fn error_code<M: ToString>(message: M, code: C) -> Self {
         Self::Error {
             message: message.to_string(),
-            code: Some(code)
+            code: Some(code),
         }
     }
 
     pub fn fail<M: ToString>(message: M) -> Self {
         Self::Fail {
             message: message.to_string(),
-            code: None
+            code: None,
         }
     }
 
     pub fn fail_code<M: ToString>(message: M, code: C) -> Self {
         Self::Fail {
             message: message.to_string(),
-            code: Some(code)
+            code: Some(code),
         }
     }
 }
 
-impl<D, C> Brest<D, C> where D: Serialize + Debug, C: num_traits::PrimInt + Serialize + Debug {
+impl<D, C> Brest<D, C>
+where
+    D: Serialize + Debug,
+    C: num_traits::PrimInt + Serialize + Debug,
+{
     #[inline]
     #[track_caller]
-    pub fn unwrap(self) -> D
-    {
+    pub fn unwrap(self) -> D {
         match self {
-            Self::Success { data } => data,
-            Self::Error {
-                message,
-                code,
-            } => unwrap_failed(
+            Self::Success(data) => data,
+            Self::Error { message, code } => unwrap_failed(
                 "called `RJSend::unwrap()` on an `Error` value",
-                &ErrorFields {
-                    message,
-                    code,
-                },
+                &ErrorFields { message, code },
             ),
-            Self::Fail {
-                message,
-                code
-            } => {
-                unwrap_failed(
-                    "called `RJSend::unwrap()` on a `Fail` value",
-                    &ErrorFields {
-                        message,
-                        code,
-                    })
-            },
+            Self::Fail { message, code } => unwrap_failed(
+                "called `RJSend::unwrap()` on a `Fail` value",
+                &ErrorFields { message, code },
+            ),
         }
     }
 
     #[inline]
     #[track_caller]
-    pub fn unwrap_error(self) -> ErrorFields<C>
-    {
+    pub fn unwrap_error(self) -> ErrorFields<C> {
         match self {
-            Self::Error {
-                message,
-                code,
-            } => ErrorFields {
-                message,
-                code,
-            },
-            Self::Success {
-                data
-            } => unwrap_failed(
+            Self::Error { message, code } => ErrorFields { message, code },
+            Self::Success(data) => unwrap_failed(
                 "called `RJSend::unwrap_error()` on an `Success` value",
                 &data,
             ),
-            Self::Fail {
-                message,
-                code
-            } => {
-                unwrap_failed(
-                    "called `RJSend::unwrap_error()` on a `Fail` value",
-                    &ErrorFields {
-                        message,
-                        code,
-                    })
-            },
+            Self::Fail { message, code } => unwrap_failed(
+                "called `RJSend::unwrap_error()` on a `Fail` value",
+                &ErrorFields { message, code },
+            ),
         }
     }
 
     #[inline]
     #[track_caller]
-    pub fn unwrap_fail(self) -> ErrorFields<C>
-    {
+    pub fn unwrap_fail(self) -> ErrorFields<C> {
         match self {
-            Self::Fail {
-                message,
-                code,
-            } => ErrorFields {
-                message,
-                code,
-            },
-            Self::Error {
-                message,
-                code
-            } => {
-                unwrap_failed(
-                    "called `RJSend::unwrap_fail()` on a `Error` value",
-                    &ErrorFields {
-                        message,
-                        code,
-                    })
-            },
-            Self::Success {
-                data
-            } => unwrap_failed(
+            Self::Fail { message, code } => ErrorFields { message, code },
+            Self::Error { message, code } => unwrap_failed(
+                "called `RJSend::unwrap_fail()` on a `Error` value",
+                &ErrorFields { message, code },
+            ),
+            Self::Success(data) => unwrap_failed(
                 "called `RJSend::unwrap_fail()` on an `Success` value",
                 &data,
             ),
@@ -155,7 +117,7 @@ impl<D, C> Brest<D, C> where D: Serialize + Debug, C: num_traits::PrimInt + Seri
     #[inline]
     pub fn unwrap_or(self, default: D) -> D {
         match self {
-            Self::Success { data } => data,
+            Self::Success(data) => data,
             _ => default,
         }
     }
@@ -166,7 +128,7 @@ impl<D, C> Brest<D, C> where D: Serialize + Debug, C: num_traits::PrimInt + Seri
         F: FnOnce() -> D,
     {
         match self {
-            Self::Success { data } => data,
+            Self::Success(data) => data,
             _ => f(),
         }
     }
@@ -191,91 +153,38 @@ impl<D, C> Brest<D, C> where D: Serialize + Debug, C: num_traits::PrimInt + Seri
     }
 }
 
-impl<D, C> Brest<D, C> where D: Serialize + Debug, C: num_traits::PrimInt + Serialize + Debug {
+impl<D, C> Brest<D, C>
+where
+    D: Serialize + Debug,
+    C: num_traits::PrimInt + Serialize + Debug,
+{
     #[inline]
     #[track_caller]
-    pub fn expect(self, msg: &str) -> D
-    {
+    pub fn expect(self, msg: &str) -> D {
         match self {
-            Self::Success { data } => data,
-            Self::Fail {
-                message,
-                code,
-            } => unwrap_failed(
-                msg,
-                &ErrorFields {
-                    message,
-                    code,
-                },
-            ),
-            Self::Error {
-                message,
-                code,
-            } => unwrap_failed(
-                msg,
-                &ErrorFields {
-                    message,
-                    code,
-                },
-            ),
+            Self::Success(data) => data,
+            Self::Fail { message, code } => unwrap_failed(msg, &ErrorFields { message, code }),
+            Self::Error { message, code } => unwrap_failed(msg, &ErrorFields { message, code }),
         }
     }
 
     #[inline]
     #[track_caller]
-    pub fn expect_error(self, msg: &str) -> ErrorFields<C>
-    {
+    pub fn expect_error(self, msg: &str) -> ErrorFields<C> {
         match self {
-            Self::Error {
-                message,
-                code,
-            } => ErrorFields {
-                message,
-                code,
-            },
-            Self::Success { data } => unwrap_failed(
-                msg,
-                &data,
-            ),
-            Self::Fail {
-                message,
-                code,
-            } => unwrap_failed(
-                msg,
-                &ErrorFields {
-                    message,
-                    code,
-                },
-            ),
+            Self::Error { message, code } => ErrorFields { message, code },
+            Self::Success(data) => unwrap_failed(msg, &data),
+            Self::Fail { message, code } => unwrap_failed(msg, &ErrorFields { message, code }),
         }
     }
 
     #[inline]
     #[track_caller]
-    pub fn expect_fail(self, msg: &str) -> ErrorFields<C>
-    {
+    pub fn expect_fail(self, msg: &str) -> ErrorFields<C> {
         match self {
-            Self::Fail {
-                message,
-                code,
-            } => ErrorFields {
-                message,
-                code,
-            },
-            Self::Error {
-                message,
-                code,
-            } => unwrap_failed(
-                msg,
-                &ErrorFields {
-                    message,
-                    code,
-                },
-            ),
-            Self::Success { data } => unwrap_failed(
-                msg,
-                &data,
-            ),
+            Self::Fail { message, code } => ErrorFields { message, code },
+            Self::Error { message, code } => unwrap_failed(msg, &ErrorFields { message, code }),
+            Self::Success(data) => unwrap_failed(msg, &data),
         }
     }
 }
@@ -287,7 +196,11 @@ fn unwrap_failed(msg: &str, error: &dyn fmt::Debug) -> ! {
     panic!("{}: {:?}", msg, error)
 }
 
-impl<D, C> Brest<D, C> where D: Serialize, C: num_traits::PrimInt + Serialize {
+impl<D, C> Brest<D, C>
+where
+    D: Serialize,
+    C: num_traits::PrimInt + Serialize,
+{
     #[inline]
     #[must_use]
     pub fn is_success(&self) -> bool {
@@ -298,7 +211,7 @@ impl<D, C> Brest<D, C> where D: Serialize, C: num_traits::PrimInt + Serialize {
     #[must_use]
     pub fn is_success_and<F: FnOnce(D) -> bool>(self, f: F) -> bool {
         match self {
-            Self::Success { data } => f(data),
+            Self::Success(data) => f(data),
             _ => false,
         }
     }
@@ -313,13 +226,7 @@ impl<D, C> Brest<D, C> where D: Serialize, C: num_traits::PrimInt + Serialize {
     #[must_use]
     pub fn is_fail_and<F: FnOnce(ErrorFields<C>) -> bool>(self, f: F) -> bool {
         match self {
-            Self::Fail { 
-                message,
-                code
-            } => f(ErrorFields {
-                message,
-                code,
-            }),
+            Self::Fail { message, code } => f(ErrorFields { message, code }),
             _ => false,
         }
     }
@@ -334,25 +241,27 @@ impl<D, C> Brest<D, C> where D: Serialize, C: num_traits::PrimInt + Serialize {
     #[must_use]
     pub fn is_error_and<F: FnOnce(ErrorFields<C>) -> bool>(self, f: F) -> bool {
         match self {
-            Self::Error {
-                message,
-                code,
-            } => f(ErrorFields {
-                message,
-                code,
-            }),
+            Self::Error { message, code } => f(ErrorFields { message, code }),
             _ => false,
         }
     }
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct ErrorFields<C> where C: num_traits::PrimInt + Serialize {
+pub struct ErrorFields<C>
+where
+    C: num_traits::PrimInt + Serialize,
+{
     pub message: String,
     pub code: Option<C>,
 }
 
-impl<D, C, E> From<Result<D, E>> for Brest<D, C> where D: Serialize, C: num_traits::PrimInt + Serialize, E: Display {
+impl<D, C, E> From<Result<D, E>> for Brest<D, C>
+where
+    D: Serialize,
+    C: num_traits::PrimInt + Serialize,
+    E: Display,
+{
     fn from(value: Result<D, E>) -> Self {
         match value {
             Ok(data) => Self::success(data),
@@ -361,7 +270,12 @@ impl<D, C, E> From<Result<D, E>> for Brest<D, C> where D: Serialize, C: num_trai
     }
 }
 
-impl<D, C, E> From<(Result<D, E>, C)> for Brest<D, C> where D: Serialize, C: num_traits::PrimInt + Serialize, E: Display {
+impl<D, C, E> From<(Result<D, E>, C)> for Brest<D, C>
+where
+    D: Serialize,
+    C: num_traits::PrimInt + Serialize,
+    E: Display,
+{
     fn from(value: (Result<D, E>, C)) -> Self {
         match value.0 {
             Ok(data) => Self::success(data),
